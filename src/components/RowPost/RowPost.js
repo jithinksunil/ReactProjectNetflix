db1e145a8ee580ad0ee96a4f3fc90d45bfd817ec
Jithinksunil <jithinksunil96@gmail.com>
2023-02-08 16:06:14 +0530

    final

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,23 +1,28 @@
 import React,{useState,useEffect} from 'react'
 import './RowPost.css'
 import axios from '../../axios'
-import {imageUrl} from '../../constants/constants'
+import {imageUrl,apiKey} from '../../constants/constants'
 
 function RowPost(props) {
   const [movies,setMOvies]=useState([])
+  const [trailer,setTrailer]=useState('')
   useEffect(()=>{
     axios.get(props.url).then((response)=>{
-    // console.log(response.data.results)
     setMOvies(response.data.results)
   })
   },[])
+  function movieTrailerHandler(id){
+    axios.get(`/movie/${id}/videos?api_key=${apiKey}&language=en-US`).then((response)=>{
+      setTrailer(response.data.results[0].key);
+    })
+  }
   return (
     <div className='row'>
         <h2>{props.title}</h2>
         <div className='posters'>
         {
           movies.map((item,index)=>{
-            return (<img className={props.isSmall?'smallPoster':'poster'} alt='poster' src={`${imageUrl+item.backdrop_path}`} />)
+            return (<img onClick={()=>{movieTrailerHandler(item.id)}} className={props.isSmall?'smallPoster':'poster'} alt='poster' src={`${imageUrl+item.backdrop_path}`} />)
           })
         }
         </div>
