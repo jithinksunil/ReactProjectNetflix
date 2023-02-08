a5a7784139de7d231b021fac6573d19d6d95a48c
Jithinksunil <jithinksunil96@gmail.com>
2023-02-08 20:12:09 +0530

    completed

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -2,10 +2,10 @@ import React,{useState,useEffect} from 'react'
 import './RowPost.css'
 import axios from '../../axios'
 import {imageUrl,apiKey} from '../../constants/constants'
-
+import YouTube from 'react-youtube'
 function RowPost(props) {
   const [movies,setMOvies]=useState([])
-  const [trailer,setTrailer]=useState('')
+  const [trailer,setTrailer]=useState()
   useEffect(()=>{
     axios.get(props.url).then((response)=>{
     setMOvies(response.data.results)
@@ -13,9 +13,21 @@ function RowPost(props) {
   },[])
   function movieTrailerHandler(id){
     axios.get(`/movie/${id}/videos?api_key=${apiKey}&language=en-US`).then((response)=>{
-      setTrailer(response.data.results[0].key);
+      if(response.data.results.length!=0){
+        setTrailer(response.data.results[0].key);
+      }else{
+        console.log('Array is empty')
+      }
     })
   }
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className='row'>
         <h2>{props.title}</h2>
@@ -26,6 +38,10 @@ function RowPost(props) {
           })
         }
         </div>
+        {
+          trailer&&<YouTube opts={opts} videoId={trailer}/>
+        }
+        
     </div>
   )
 }
